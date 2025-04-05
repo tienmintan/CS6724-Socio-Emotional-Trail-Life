0aaac4bed7c05d35f9b74a3db05aea2e2afcb5ef
Afreen Ali <afreen21@vt.edu>
2025-04-05 01:37:20 -0400

    changed text size

--- Pages/Visualizations.py
+++ Pages/Visualizations.py
@@ -20,12 +20,17 @@ tab_titles = [
 selected_tab = st.sidebar.radio("Select a Team Member", tab_titles)
 
 if selected_tab == "Afreen":
-    st.write("Content by Afreen")
+    st.markdown("# **Content for Afreen**")
+    st.write("Afreen's detailed information here.")
 elif selected_tab == "Shamsia":
-    st.write("Content by Shamsia")
+    st.markdown("# **Content for Shamsia**")
+    st.write("Shamsia's detailed information here.")
 elif selected_tab == "Emily":
-    st.write("Content by Emily")
+    st.markdown("# **Content for Emily**")
+    st.write("Emily's detailed information here.")
 elif selected_tab == "Krish":
-    st.write("Content by Krish")
+    st.markdown("# **Content for Krish**")
+    st.write("Krish's detailed information here.")
 elif selected_tab == "Tien Man":
-    st.write("Content by Tien Man")
+    st.markdown("# **Content for Tien Man**")
+    st.write("Tien Man's detailed information here.")
