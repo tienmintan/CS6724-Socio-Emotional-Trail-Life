bcf0cb97faeae66f079b68fbcbc48cc922b8df72
Afreen Ali <afreen21@vt.edu>
2025-04-05 00:56:24 -0400

    changed main file to welcome homepage

---
+++ Welcome.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome",  
+    page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("Socio-Emotional Trail Analysis")  
