504ccb78121999cbe7b29d974ef0edc49c5ebfb1
Afreen Ali <afreen21@vt.edu>
2025-04-07 10:20:40 -0400

    updated Welcome page

--- Welcome.py
+++ Welcome.py
@@ -14,6 +14,7 @@ col1, col2 = st.columns([1, 2])
 
 with col1:
     st.header("Project Description")
+    st.image("https://images.unsplash.com/photo-1558483754-4618fc25fe5e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", use_column_width=True)
 
 with col2:
     st.write("""
