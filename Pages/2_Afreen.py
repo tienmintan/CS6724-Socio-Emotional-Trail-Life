eb5240c1e59bae5004eac707ab1727d501b4871d
Afreen Ali <afreen21@vt.edu>
2025-04-07 11:43:25 -0400

    updated Afreen.py

--- Pages/2_Afreen.py
+++ Pages/2_Afreen.py
@@ -20,30 +20,53 @@ tabs = st.tabs(tab_titles)
 
 
 with tabs[0]:
-    st.header("Best and Worst Spots in Each State")
-    st.write("📍 ")
-    
-    with open("best and worst spots.html", "r", encoding="utf-8") as file:
-        map_html = file.read()
+    st.header("Best and Worst Spots in Each State📍")
+    col1, col2 = st.columns([2,1])
+    with col1:
+        with open("best and worst spots.html", "r", encoding="utf-8") as file:
+            map_html = file.read()
+        components.html(map_html, height=600, scrolling=True)
 
-    components.html(map_html, height=600, scrolling=True)
-    
+    with col2:
+        with st.container(border=True):
+            st.subheader("Map Explanation:")
+            st.write("This map displays the 3 best and worst 3 campsites, hostels, or shelters to visit on the Appalachian trail. Based on the emotional sentiment of journal entries recorded by hikers, the locations were measured and ranked by state for their total joy and negativity.")
+
+            st.subheader("Key Insights:")
+            st.write("Zoom into the map and click on the markers to see the worst locations, denoted by the red markers, and the best locations, denoted by the blue markers. West Virginia only had one location that had a higher positive weight, so it only has one good visitation site. This shows there is a need for better campsites or shelters within the state for hikers.")
+
+
 
 with tabs[1]:
-    st.header("Trail Magic Occurences by State")
-    st.write("🎒 ")
+    st.header("Trail Magic Occurences by State 🪄")
+    col1, col2 = st.columns([2,1])
+    with col1:
+        with open("trail magic state map.html", "r", encoding="utf-8") as file:
+            map_html = file.read()
+        components.html(map_html, height=600, scrolling=True)
 
-    with open("trail magic state map.html", "r", encoding="utf-8") as file:
-        map_html = file.read()
+    with col2:
+        with st.container(border=True):
+            st.subheader("Map Explanation:")
+            st.write("This map displays how much trail magic occurs in each of the 14 states on the trail. Trail magic refers to acts of kindness towards hikers, including finding food or shelters with the help of strangers or other hikers.")
 
-    components.html(map_html, height=600, scrolling=True)
+            st.subheader("Key Insights:")
+            st.write("Zoom into the map to view which states contain the most trail magic. The darker the blue, the more magical the state is! Virginia has the most trail magic, while West Virginia has the least. This corresponds to our first map with WV's lack of good locations.")
+
 
 
 with tabs[2]:
-    st.header("Negative Emotions Across the Trail")
-    st.write("😞 ")
+    st.header("Negative Emotions Across the Trail 👎")
+    col1, col2 = st.columns([2,1])
+    with col1:
+        with open("negative emotions map.html", "r", encoding="utf-8") as file:
+            map_html = file.read()
+        components.html(map_html, height=600, scrolling=True)
 
-    with open("negative emotions map.html", "r", encoding="utf-8") as file:
-        map_html = file.read()
+    with col2:
+        with st.container(border=True):
+            st.subheader("Map Explanation:")
+            st.write("This map displays the negative emotions that have been recorded on the trail. It specifically maps sadess, disgust, anger, and fear, which were expressed via journal entries from the hikers.")
 
-    components.html(map_html, height=600, scrolling=True)
+            st.subheader("Key Insights:")
+            st.write("Zoom into the map to view the individual emotion markers. Hovering over a marker will provide which emotion it corresponds to since the trail is densely packed with markerts. The most common negative emotion expressed on the trail seems to be fear, with sadness as a close second.")
--- Welcome.py
+++ Welcome.py
@@ -39,35 +39,5 @@ with cont2:
         </div>
         """, unsafe_allow_html=True
     )
-        
 
 
-# row1_col = st.columns((1, 1))
-
-# col1, col2 = st.columns(2)
-
-# with row1_col[0]:
-#     st.header("Project Description")
-#     st.image("https://images.unsplash.com/photo-1558483754-4618fc25fe5e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", use_container_width=True)
-
-# with row1_col[1]:
-#     st.markdown(
-#         """
-#         <div style="font-size: 20px;">
-#         Emotional well-being is vital for individuals experiencing a life transition journey, 
-#         as it helps them maintain resilience, stay focused, and inform their decision-making. 
-
-#         Given the increasing integration of technology into everyday life, even in natural settings, 
-#         individuals share their experiences in online media, especially community-centered platforms. 
-
-#         For instance, Appalachian Trail long-distance hikers share their adventurous and emotional journey 
-#         through community blogs, necessitating an understanding of contributing factors to their emotions. 
-
-#         This project explores how social connections relate to hikers' emotional states amidst the uncertainties 
-#         of a dynamic socio-technical-natural system by leveraging AI-driven techniques and machine learning 
-#         algorithms on large-scale data.
-#         </div>
-#         """, unsafe_allow_html=True
-#     )
-    
-    
