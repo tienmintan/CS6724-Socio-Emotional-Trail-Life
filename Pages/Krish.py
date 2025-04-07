598e8e7cd05980e17679a74c73655a1d471f1637
Afreen Ali <afreen21@vt.edu>
2025-04-06 23:45:27 -0400

    added team member pages

---
+++ Pages/Krish.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Visualizations",  
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("Visualizations Made By Krish")  
