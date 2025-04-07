b1636232bc0c5638f1b0415c6c027809df237876
Afreen Ali <afreen21@vt.edu>
2025-04-07 10:35:02 -0400

    updated Welcome page

--- Welcome.py
+++ Welcome.py
@@ -17,22 +17,13 @@ with col1:
     st.image("https://images.unsplash.com/photo-1558483754-4618fc25fe5e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", use_container_width=True)
 
 with col2:
-    st.markdown("""
-    <p style="font-size: 20px;">
-    Emotional well-being is vital for individuals experiencing a life transition journey, 
-    as it helps them maintain resilience, stay focused, and inform their decision-making. 
-
-    Given the increasing integration of technology into everyday life, even in natural settings, 
-    individuals share their experiences in online media, especially community-centered platforms. 
-
-    For instance, Appalachian Trail long-distance hikers share their adventurous and emotional journey 
-    through community blogs, necessitating an understanding of contributing factors to their emotions. 
-
-    This project explores how social connections relate to hikers' emotional states amidst the uncertainties 
-    of a dynamic socio-technical-natural system by leveraging AI-driven techniques and machine learning 
-    algorithms on large-scale data.
-    </p>
-    """)
+    st.markdown(""" #### Emotional well-being is vital for individuals experiencing a life transition journey, as it helps them maintain resilience, stay focused, and inform their decision-making. 
+    
+Given the increasing integration of technology into everyday life, even in natural settings, individuals share their experiences in online media, especially community-centered platforms. 
+                
+For instance, Appalachian Trail long-distance hikers share their adventurous and emotional journey through community blogs, necessitating an understanding of contributing factors to their emotions. 
+                
+This project explores how social connections relate to hikers' emotional states amidst the uncertainties of a dynamic socio-technical-natural system by leveraging AI-driven techniques and machine learning algorithms on large-scale data. """)
 
 col1.empty()
 col2.empty()
