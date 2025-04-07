ac188660d3f035374c4c0fbfb2e375600b6941d3
Afreen Ali <afreen21@vt.edu>
2025-04-07 00:31:35 -0400

    updated Afreen's file

--- Pages/2_Afreen.py
+++ Pages/2_Afreen.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Visualizations",  
@@ -21,11 +22,28 @@ tabs = st.tabs(tab_titles)
 with tabs[0]:
     st.header("Best and Worst Spots in Each State")
     st.write("📍 ")
+    
+    with open("best and worst spots.html", "r", encoding="utf-8") as file:
+        map_html = file.read()
+
+    components.html(map_html, height=600, scrolling=True)
+    
 
 with tabs[1]:
     st.header("Trail Magic Occurences by State")
     st.write("🎒 ")
 
+    with open("trail magic state map.html", "r", encoding="utf-8") as file:
+        map_html = file.read()
+
+    components.html(map_html, height=600, scrolling=True)
+
+
 with tabs[2]:
     st.header("Negative Emotions Across the Trail")
     st.write("😞 ")
+
+    with open("negative emotions map", "r", encoding="utf-8") as file:
+        map_html = file.read()
+
+    components.html(map_html, height=600, scrolling=True)
